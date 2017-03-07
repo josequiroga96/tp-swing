4427c238f202511f3e619099b6216eb0a85d62cc
joserojas69 <jose.rojas@ing.austral.edu.ar>
2017-03-07 01:02:32 -0300

    Update Mainnnnnn.java

--- src/Mainnnnnn.java
+++ src/Mainnnnnn.java
@@ -1,9 +1,11 @@
 /**
  * Created by JoseRojas on 6/3/17.
  */
+/*
 public class Mainnnnnn {
     public static void main(String[] args){
         WindowMainnnnnn wM = new WindowMainnnnnn("Mainnnnnn Menu");
         //wM.pack();
     }
 }
+*/
